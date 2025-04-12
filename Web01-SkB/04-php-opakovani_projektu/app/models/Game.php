6d5610f8154c4da8c9c221747e0b26568a6ef82d
Vojtěch Macháček <vojtech.machacek@tul.cz>
2025-04-12 11:55:19 +0200

    #13 procvičení editace a mazání záznamů

--- Web01-SkB/04-php-opakovani_projektu/app/models/Game.php
+++ Web01-SkB/04-php-opakovani_projektu/app/models/Game.php
@@ -12,7 +12,6 @@ class Game {
                 VALUES (:title, :developer, :genre, :platform, :release_year, :price, :pegi, :description, :link, :images)";
         
         $stmt = $this->db->prepare($sql);
-        
         return $stmt->execute([
             ':title' => $title,
             ':developer' => $developer,
@@ -32,5 +31,46 @@ class Game {
         $stmt = $this->db->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getById($id) {
+        $sql = "SELECT * FROM games WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([':id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function update($id, $title, $developer, $genre, $platform, $release_year, $price, $pegi, $description, $link) {
+        $sql = "UPDATE games 
+                SET title = :title,
+                    developer = :developer,
+                    genre = :genre,
+                    platform = :platform,
+                    release_year = :release_year,
+                    price = :price,
+                    pegi = :pegi,
+                    description = :description,
+                    link = :link
+                WHERE id = :id";
+    
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([
+            ':id' => $id,
+            ':title' => $title,
+            ':developer' => $developer,
+            ':genre' => $genre,
+            ':platform' => $platform,
+            ':release_year' => $release_year,
+            ':price' => $price,
+            ':pegi' => $pegi,
+            ':description' => $description,
+            ':link' => $link
+        ]);
+    }
+
+    public function delete($id) {
+        $sql = "DELETE FROM games WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([':id' => $id]);
     }
 }
